ff739645243739d922adc96ca2a6178c348e0787
Juankpr0 <Camitrianasuarez@gmail.com>
2025-10-01 08:00:53 +0200

    Categoria adapter

--- app/src/main/java/com/example/appinterface/Adapter/CategoriaAdapter.kt
+++ app/src/main/java/com/example/appinterface/Adapter/CategoriaAdapter.kt
@@ -3,15 +3,26 @@ package com.example.appinterface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CategoriaAdapter(private var categorias: List<Categoria>) :
-    RecyclerView.Adapter<CategoriaAdapter.CategoriaViewHolder>() {
+class CategoriaAdapter(
+    private var categorias: List<Categoria>,
+    private val listener: OnCategoriaClickListener
+) : RecyclerView.Adapter<CategoriaAdapter.CategoriaViewHolder>() {
+
+    interface OnCategoriaClickListener {
+        fun onEditarClick(categoria: Categoria)
+        fun onEliminarClick(categoria: Categoria)
+    }
+
 
     class CategoriaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nombre: TextView = itemView.findViewById(R.id.txtNombreCategoria)
         val descripcion: TextView = itemView.findViewById(R.id.txtDescripcionCategoria)
+        val btnEditar: Button = itemView.findViewById(R.id.btnEditarItem)
+        val btnEliminar: Button = itemView.findViewById(R.id.btnEliminarItem)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriaViewHolder {
@@ -22,10 +33,11 @@ class CategoriaAdapter(private var categorias: List<Categoria>) :
 
     override fun onBindViewHolder(holder: CategoriaViewHolder, position: Int) {
         val categoria = categorias[position]
-        holder.apply {
-            nombre.text = categoria.nombre
-            descripcion.text = categoria.descripcion ?: "Sin descripción"
-        }
+        holder.nombre.text = categoria.nombre
+        holder.descripcion.text = categoria.descripcion ?: "Sin descripción"
+
+        holder.btnEditar.setOnClickListener { listener.onEditarClick(categoria) }
+        holder.btnEliminar.setOnClickListener { listener.onEliminarClick(categoria) }
     }
 
     override fun getItemCount(): Int = categorias.size
